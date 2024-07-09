d12ac7c77528f1e99b0fc5d362951722b55841ef
zianzahid <ziamustafa2000@hotmail.com>
2024-07-08 18:52:55 -0700

    commit my first git

---
+++ src/JavaIsEasy/HashMapExample.java
@@ -0,0 +1,20 @@
+package JavaIsEasy;
+
+
+    import java.util.HashMap;
+
+    public class HashMapExample {
+
+        public static void main(String[] args) {
+            // Create a HashMap instance
+            HashMap<Integer, String> myMap = new HashMap<>();
+
+            // Add a key-value pair to the HashMap
+            myMap.put(1, "Zia");
+
+            // Print the value associated with key 1
+            System.out.println("Key 1: " + myMap.get(1));
+        }
+    }
+
+
